d005e43f30afb3a711f763db04681ac4dcece1f7
ChileKasoka <chilekesha.kasoka@cs.unza.zm>
2023-11-12 22:40:03 +0200

    hello world

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,15 +4,18 @@
             <img src="/me.png" alt="me" class="w-full h-full object-cover rounded-full" />
         </div>
         <div class=" text-center m-auto leading-normal space-y-5">
-            <p class=" text-gray-600 text-9xl">
+            <p class=" text-[#32363F] text-8xl">
                 Hello world,
             </p>
-            <h2 class=" text-6xl">
+            <h2 class=" text-6xl text-[#32363F]">
                 I'm Chile,
             </h2>
-            <h1 class=" text-4xl">
+            <h1 class=" text-4xl text-[#054861]">
                 Aspiring Full stack.
             </h1>
+            <button onsubmit="handleDownload()" class=" hover:bg-[#ebe8e8] text-[#054861] w-44 h-12 rounded-full bg-[#BFBBBB]">
+                Download CV
+            </button>
         </div>
     </div>
 </template>
