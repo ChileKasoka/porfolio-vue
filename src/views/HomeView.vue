6f15d0d258e7fbc0c9b7984b4326ae7ebe05663a
ChileKasoka <chilekesha.kasoka@cs.unza.zm>
2023-11-15 21:05:33 +0200

    mobile responsive optimization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
         <div class="lg:ml-64 m-auto h-64 w-64">
             <img src="/me.png" alt="me" class="w-full h-full object-cover rounded-full" />
         </div>
-        <div class=" text-center m-auto leading-normal space-y-5">
+        <div class=" text-center m-auto leading-normal p-5 space-y-5">
             <p class=" text-[#32363F] text-8xl">
                 Hello world,
             </p>
@@ -19,19 +19,19 @@
         </div>
     </div>
     <div class=" leading-normal ">
-        <h1 class="text-center">
+        <h1 class="text-center text-[#054861]">
             Get to know me better
         </h1>
-        <h3 class="text-center text-5xl">
+        <h3 class="text-4xl text-[#054861] text-center">
             About Me
         </h3>
-        <div class="flex m-auto w-4/5 min-h-screen">
+        <div class="block lg:flex m-auto w-auto min-h-screen p-5">
             <div class="m-auto h-72 w-72 mr-10">
                 <img src="/IMG_6200.png" alt="me" class="w-full h-full object-cover rounded-lg" />
             </div>
-            <div class="block m-auto w-4/5">
-                <div class="block md:flex justify-center">
-                    <div class=" block text-center w-2/6 mr-5 bg-slate-100 rounded-2xl">
+            <div class="block m-auto w-full lg:w-4/5">
+                <div class="block md:flex justify-center p-5 text-gray-500">
+                    <div class=" block text-center lg:w-2/6 m-5 md:mr-5 bg-slate-100 rounded-2xl">
                         <h2 class="p-2">
                         <b>Experience</b> 
                         </h2>
@@ -39,7 +39,7 @@
                             2 years+ <br> Frontend Experience/ Backend Technologies
                         </div>
                     </div>
-                    <div class=" block text-center w-2/6 bg-slate-100 rounded-2xl">
+                    <div class=" block text-center lg:w-2/6 m-5 bg-slate-100 rounded-2xl">
                         <h2 class="p-2">
                             Education
                         </h2>
@@ -49,10 +49,17 @@
                     </div>
                 </div>
 
-                <div class="w-2/3 text-center m-auto p-5">
-                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis illo at enim doloribus, 
-                    odit aspernatur praesentium deserunt eius dignissimos exercitationem 
-                    ipsam tenetur nemo cupiditate repellendus laudantium itaque quidem aliquam adipisci.
+                <div class="w-2/3 text-center m-auto text-lg font-normal mb-3 text-gray-500">
+                    Meet Chile, a seasoned full-stack developer with over 2 years of coding experience.
+                    On the frontend, Chile is well-versed in HTML5, CSS3, and JavaScript,
+                    utilizing popular frameworks such as React.js and Vuejs to create dynamic and
+                    responsive user interfaces. With a keen eye for design 
+                    <br><br>
+                    On the backend, I excel in server-side scripting languages like Golang, Java and Node.js,
+                    building robust and scalable applications. Database management is second nature,
+                    as Chile(Stunna) navigates through SQL and NoSQL databases like MySQL, MongoDB, and PostgreSQL.
+                    RESTful API development is a forte,
+                    allowing for smooth communication between the frontend and backend components.
                 </div>
             </div>
         </div>
