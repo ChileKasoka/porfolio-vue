28fff0edc9cdefc5320e89c581fd6f7ff026dc40
Chile <118934314+ChileKasoka@users.noreply.github.com>
2025-09-18 22:30:23 +0200

    changed toggle color

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -21,7 +21,7 @@
       </ul>
 
       <!-- Mobile Menu Button -->
-      <button @click="toggleMenu" class="lg:hidden text-slate-900 focus:outline-none">
+      <button @click="toggleMenu" class="lg:hidden text-white focus:outline-none">
         <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
             d="M4 6h16M4 12h16M4 18h16" />
